a8affec6bb519248d1f4b1cc835ac28ba3051842
dhrubobari <bari.dhrubo@gmail.com>
2022-04-17 21:35:16 +0600

    services showed in UI

--- src/components/Home/Service/Service.js
+++ src/components/Home/Service/Service.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Service.css'
 
-const Service = () => {
+const Service = ({service}) => {
+    const {name, img, description, price} = service;
     return (
-        <div>
-            
+        <div className='service-list'>
+            <img src={img} alt="" />
+            <h4 className='info'>{name}</h4>
+            <p className='info'>{description}</p>
+            <p>Hire me: ${price}</p>
         </div>
     );
 };
--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect( ()=>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
+
     return (
-        <div className='text-center'>
+        <div id="services" className='text-center'>
+            <div className=''>
             <p className='mt-5'>SERVICES</p>
             <h2>My Services</h2>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                    ></Service>)
+                }
+            </div>
+            </div>
         </div>
     );
 };
