353d8e736263c97ec824b6517fd9f5584dad59a8
dhrubobari <bari.dhrubo@gmail.com>
2022-04-17 18:45:53 +0600

    header added

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="white">
+    <Container>
+      <Navbar.Brand>Adonis and Associates</Navbar.Brand>
+    </Container>
+  </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Hello! I am Adonis.</h2>
+            <p className='text-primary'>With over 5 years experience, I can help you with all your accounting needs.</p>
+        </div>
+    );
+};
+
+export default Home;
